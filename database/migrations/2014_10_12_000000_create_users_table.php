4e8b8861b6490d3f92722c4ecbd168e4fa2b0d26
ali0aisha <u1865309@unimail.hud.ac.uk>
2021-11-05 01:32:10 +0000

    added files to gitignore. Default admin created when migrating database.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Hash;
 
 class CreateUsersTable extends Migration
 {
@@ -21,8 +22,18 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
         });
+
+        DB::table('users')->insert(
+            array(
+                'name' => env('ADMIN_NAME', 'Admin User'),
+                'email' => env('ADMIN_EMAIL', 'admin@gmail.com'),
+                'password' => Hash::make(env('ADMIN_PASSWORD', 'mastersuite')),
+                'role' => 'ADMIN',
+            )
+        );
     }
 
     /**
